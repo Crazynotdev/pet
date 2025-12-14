8d66bd2afd816f6aa7c86166346426ec448fca68
ZynXx <techigwe@gmail.com>
2025-12-14 10:26:45 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,46 +1,46 @@
-const { makeWASocket, useMultiFileAuthState } = require(’@whiskeysockets/baileys’)
-const QRCode = require(‘qrcode’)
-const handleMessage = require(’./handler’)
+const { makeWASocket, useMultiFileAuthState } = require('@whiskeysockets/baileys')
+const QRCode = require('qrcode')
+const handleMessage = require('./handler')
 
 async function startPair(number, mode) {
-const { state, saveCreds } = await useMultiFileAuthState(sessions/${number})
-const crazy = makeWASocket({ auth: state })
+    const { state, saveCreds } = await useMultiFileAuthState(`sessions/${number}`)
+    const sock = makeWASocket({ auth: state })
 
-crazy.ev.on(‘creds.update’, saveCreds)
-crazy.ev.on(‘messages.upsert’, handleMessage(sock))
+    sock.ev.on('creds.update', saveCreds)
+    sock.ev.on('messages.upsert', handleMessage(sock))
 
-if (mode === ‘pair’ && !state.creds.registered) {
-try {
-const code = await crazy.requestPairingCode(number)
-return { type: ‘pair’, data: code }
-} catch (err) {
-console.error(‘Erreur Pairing Code:’, err)
-return { type: ‘error’, data: err.message }
-}
-}
+    if (mode === 'pair' && !state.creds.registered) {
+        try {
+            const code = await sock.requestPairingCode(number)
+            return { type: 'pair', data: code }
+        } catch (err) {
+            console.error('Erreur Pairing Code:', err)
+            return { type: 'error', data: err.message }
+        }
+    }
 
-if (mode === ‘qr’) {
-return new Promise((resolve, reject) => {
-const timeout = setTimeout(() => reject(new Error(‘QR timeout’)), 15000)
-crazy.ev.on(‘connection.update’, async (update) => {
-try {
-if (update.qr) {
-clearTimeout(timeout)
-const qrImage = await QRCode.toDataURL(update.qr)
-resolve({ type: ‘qr’, data: qrImage })
-}
-if (update.connection === ‘close’) {
-console.log(‘Connexion fermée:’, update.lastDisconnect?.error)
-}
-if (update.connection === ‘open’) {
-console.log(‘Connecté à WhatsApp’)
-}
-} catch (err) {
-reject(err)
-}
-})
-})
-}
+    if (mode === 'qr') {
+        return new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => reject(new Error('QR timeout')), 15000)
+            sock.ev.on('connection.update', async (update) => {
+                try {
+                    if (update.qr) {
+                        clearTimeout(timeout)
+                        const qrImage = await QRCode.toDataURL(update.qr)
+                        resolve({ type: 'qr', data: qrImage })
+                    }
+                    if (update.connection === 'close') {
+                        console.log('Connexion fermée:', update.lastDisconnect?.error)
+                    }
+                    if (update.connection === 'open') {
+                        console.log('Connecté à WhatsApp')
+                    }
+                } catch (err) {
+                    reject(err)
+                }
+            })
+        })
+    }
 }
 
 module.exports = { startPair }
